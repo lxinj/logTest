45d1742c32dc6a1077e3a1c519be381b0e972c9a
lxinj <991020364@qq.com>
2022-06-16 00:32:23 +0800

    init project 20220615

---
+++ src/test/java/com/lxj/logtest/LogTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lxj.logtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
